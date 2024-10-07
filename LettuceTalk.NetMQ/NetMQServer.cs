8ee6c60e2cfe8ebd11bfaf618205cddc37e6be36
TheKingOfLettuce <68159617+TheKingOfLettuce@users.noreply.github.com>
2024-10-06 22:01:26 -0400

    client registers + comments

--- LettuceTalk.NetMQ/NetMQServer.cs
+++ LettuceTalk.NetMQ/NetMQServer.cs
@@ -6,6 +6,9 @@ using System.Text;
 
 namespace LettuceTalk.NetMQ;
 
+/// <summary>
+/// Send message args that contains an arugment for the ClientID to send to
+/// </summary>
 public class SendClientMessageArgs : SendMessageArgs {
     public readonly string ClientID;
 
@@ -17,6 +20,9 @@ public class SendClientMessageArgs : SendMessageArgs {
     }
 }
 
+/// <summary>
+/// A server that uses a <see cref="RouterSocket"/> to handle client connections
+/// </summary>
 public class NetMQServer : IDisposable {
     public event Action<string>? OnClientRegistered;
 
@@ -43,6 +49,10 @@ public class NetMQServer : IDisposable {
         Dispose(false);
     }
 
+    /// <summary>
+    /// Send a message to a specific client, use empty string to the ClientID to send to all
+    /// </summary>
+    /// <param name="args"></param>
     public void SendMessage(SendClientMessageArgs args) {
         // if empty string, send to all clients
         if (args.ClientID == string.Empty) {
@@ -55,6 +65,41 @@ public class NetMQServer : IDisposable {
         }
     }
 
+    /// <summary>
+    /// Registers a client with the given ID with a default callback instance
+    /// </summary>
+    /// <param name="clientID">the ID to register with</param>
+    public void RegisterClient(string clientID) => RegisterClient(clientID, new MessageCallbackHandler());
+
+    /// <summary>
+    /// Registers a client with the given ID and callback instance
+    /// </summary>
+    /// <param name="clientID">the ID to register with</param>
+    /// <param name="callbackHandler">the message callback handler to associate with</param>
+    public void RegisterClient(string clientID, MessageCallbackHandler callbackHandler) {
+        if (_clients.ContainsKey(clientID))
+            throw new ArgumentException($"Received duplicate client ID during registration: {clientID}");
+        _clients[clientID] = callbackHandler;
+        OnClientRegistered?.Invoke(clientID);
+    }
+
+    /// <summary>
+    /// Gets a callback instance associated with the client ID
+    /// </summary>
+    /// <param name="clientID">the client ID to check</param>
+    /// <returns>the message callback instance associated with the client</returns>
+    public MessageCallbackHandler GetClientCallbackHandler(string clientID) {
+        if (!_clients.ContainsKey(clientID))
+            throw new ArgumentException("ClientID is not registered to the server");
+
+        return _clients[clientID];
+    }
+
+    /// <summary>
+    /// Handles when a message is ready to process and send in the queue
+    /// </summary>
+    /// <param name="sender">unused</param>
+    /// <param name="queueArgs">unused but could be used to get the queue</param>
     protected void HandleSendMessage(object? sender, NetMQQueueEventArgs<SendClientMessageArgs> queueArgs) {
         if (!_messageQueue.TryDequeue(out SendClientMessageArgs args, TimeSpan.FromSeconds(5)) || args == default) {
             throw new Exception("Failed to dequeue send message for server");
@@ -72,15 +117,17 @@ public class NetMQServer : IDisposable {
             throw new Exception($"Failed to send message to {args.ClientID}");    
     }
 
+    /// <summary>
+    /// Handles when the socket receives a message and is ready to process it
+    /// </summary>
+    /// <param name="sender">unused</param>
+    /// <param name="args">the message data</param>
     protected void HandleMessageReceived(object? sender, NetMQSocketEventArgs args) {
         NetMQMessage messageData = args.Socket.ReceiveMultipartMessage(3);
         string clientID = Encoding.Unicode.GetString(messageData[0].Buffer);
         Message message = MessageFactory.GetMessage(messageData[2].Buffer);
         if (message.GetType() == typeof(RegisterClient)) {
-            if (_clients.ContainsKey(clientID))
-                throw new ArgumentException($"Received duplicate client ID during registration: {clientID}");
-            _clients[clientID] = new MessageCallbackHandler();
-            OnClientRegistered?.Invoke(clientID);
+            RegisterClient(clientID);
         }
         else if (_clients.ContainsKey(clientID)) {
             _clients[clientID].Publish(message);
